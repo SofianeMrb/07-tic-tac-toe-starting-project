11f897dd906e69db43ff08c78f57de3236951c23
SofianeMrb <136330945+SofianeMrb@users.noreply.github.com>
2024-01-24 16:51:22 +0100

    misc : code cleanup

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -4,25 +4,7 @@
 
 
   export default function GameBoard({onSelectSquare , board}){
-    
 
-    
-
-
-
-/* const [gameBoard, setGameBoard] = useState(initialGameBoard);
-    
-
-
-function handleClick(rowIndex, colIndex){
-    setGameBoard(prevGameBoard => {
-    const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    newGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-    return newGameBoard;
-    })
-    onSelectSquare();
-  } */
-    
     return (<ol id="game-board">
         
         {board.map((row, rowIndex) => <li key={rowIndex}>
